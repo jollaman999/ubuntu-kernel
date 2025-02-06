34e724ef7d8cc87d1b7a296088fdffb1e9dc7010
Tobias Heider <tobias.heider@canonical.com>
2025-02-06 16:34:15 +0100

    UBUNTU: SAUCE: firmware: qcom: scm: Allow QSEECOM on Acer Swift 14 models
    BugLink: https://bugs.launchpad.net/bugs/2100858
    
    Add the aforementioned machines to the list to get e.g. efivars up.
    
    Signed-off-by: Tobias Heider <tobias.heider@canonical.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2300,6 +2300,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_qseecom_app_send);
  + any potential issues with this, only allow validated machines for now.
  */
 static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
+	{ .compatible = "acer,swift-sf14-11" },
 	{ .compatible = "asus,vivobook-s15" },
 	{ .compatible = "asus,vivobook-s15-x1p4" },
 	{ .compatible = "asus,zenbook-a14-ux3407qa" },
